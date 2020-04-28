1efdd158416a628b911885b8ea126507e61d6198
Zi Yin <yinzi.aiesecjinan@gmail.com>
2020-04-28 09:41:39 +0100

    myapp: change exchange name to a variable

--- myapp.py
+++ myapp.py
@@ -20,12 +20,14 @@ app = dash.Dash()
 
 
 # data
-
+exchange_name = 'bitbay'
 conn = sqlite3.connect(
     '/Users/Zi/Projects/Dash_eth/test.db', check_same_thread=False)
-df1 = pd.read_sql_query("SELECT * from indodax_merge_td_bal_ods", conn)
-df2 = pd.read_sql_query("SELECT * from indodax_open_order", conn)
-df3 = pd.read_sql_query("SELECT * from indodax_trades", conn)
+df1 = pd.read_sql_query(
+    "SELECT * from {}_merge_td_bal_ods".format(exchange_name), conn)
+df2 = pd.read_sql_query(
+    "SELECT * from {}_open_order".format(exchange_name), conn)
+df3 = pd.read_sql_query("SELECT * from {}_trades".format(exchange_name), conn)
 conn.close()
 
 
@@ -324,13 +326,16 @@ app.layout = html.Div(children=[
 def update_tables_graphs(n):
   conn = sqlite3.connect(
       '/Users/Zi/Projects/Dash_eth/test.db', check_same_thread=False)
-  df1 = pd.read_sql_query("SELECT * from indodax_merge_td_bal_ods", conn)
-  df2 = pd.read_sql_query("SELECT * from indodax_open_order", conn)
-  df3 = pd.read_sql_query("SELECT * from indodax_trades", conn)
+  df1 = pd.read_sql_query(
+      "SELECT * from {}_merge_td_bal_ods".format(exchange_name), conn)
+  df2 = pd.read_sql_query(
+      "SELECT * from {}_open_order".format(exchange_name), conn)
+  df3 = pd.read_sql_query(
+      "SELECT * from {}_trades".format(exchange_name), conn)
   conn.close()
   # tables
   ## tables - data
-  tbl_my_trades = df3[['time', 'type', 'type', 'takerOrMaker', 'side', 'amount', 'price',
+  tbl_my_trades = df3[['time', 'type', 'takerOrMaker', 'side', 'amount', 'price',
                        'fee_fiat', 'fee_pct']]
   tbl_my_orders = df2[['time', 'OpenOrder_side', 'OpenOrder_price',
                        'OpenOrder_amount', 'OpenOrder_filled', 'OpenOrder_remaining',
@@ -349,109 +354,6 @@ def update_tables_graphs(n):
 
   return tbl_my_orders.to_dict('records'), tbl_my_trades.to_dict('records'), columns_orders, columns_trades, fig1, fig2
 
-# server = app.server
-
-# app.scripts.config.serve_locally = False
-
-
-# colorscale = cl.scales['9']['qual']['Paired']
-
-# df = pd.read_csv(
-#     'https://raw.githubusercontent.com/plotly/datasets/master/dash-stock-ticker-demo.csv')
-
-# conn = sqlite3.connect('test.db')
-# df1 = pd.read_sql_query("SELECT * from bitbay_bal_orderbook", conn)
-# print(df1)
-
-# app.layout = html.Div([
-#     html.Div([
-#         html.H2('Finance Explorer',
-#                 style={'display': 'inline',
-#                        'float': 'left',
-#                        'font-size': '2.65em',
-#                        'margin-left': '7px',
-#                        'font-weight': 'bolder',
-#                        'font-family': 'Product Sans',
-#                        'color': "rgba(117, 117, 117, 0.95)",
-#                        'margin-top': '20px',
-#                        'margin-bottom': '0'
-#                        }),
-#         # html.Img(src="https://s3-us-west-1.amazonaws.com/plotly-tutorials/logo/new-branding/dash-logo-by-plotly-stripe.png",
-#         #         style={
-#         #             'height': '100px',
-#         #             'float': 'right'
-#         #         },
-#         # ),
-#     ]),
-#     dcc.Dropdown(
-#         id='stock-ticker-input',
-#         options=[{'label': s[0], 'value': str(s[1])}
-#                  for s in zip(df.Stock.unique(), df.Stock.unique())],
-#         value=['GOOGL'],
-#         multi=True
-#     ),
-#     html.Div(id='graphs')
-# ], className="container")
-
-
-# def bbands(price, window_size=10, num_of_std=5):
-#     rolling_mean = price.rolling(window=window_size).mean()
-#     rolling_std = price.rolling(window=window_size).std()
-#     upper_band = rolling_mean + (rolling_std * num_of_std)
-#     lower_band = rolling_mean - (rolling_std * num_of_std)
-#     return rolling_mean, upper_band, lower_band
-
-
-# @app.callback(
-#     dash.dependencies.Output('graphs', 'children'),
-#     [dash.dependencies.Input('stock-ticker-input', 'value')])
-# def update_graph(tickers):
-#     graphs = []
-
-#     if not tickers:
-#         graphs.append(html.H3(
-#             "Select a stock ticker.",
-#             style={'marginTop': 20, 'marginBottom': 20}
-#         ))
-#     else:
-#         for i, ticker in enumerate(tickers):
-
-#             dff = df1
-
-#             candlestick = {
-#                 'x': dff['time'],
-#                 'open': dff['1_bid_px'],
-#                 'high': dff['1_bid_px'],
-#                 'low': dff['1_bid_px'],
-#                 'close': dff['1_bid_px'],
-#                 'type': 'candlestick',
-#                 'name': ticker,
-#                 'legendgroup': ticker,
-#                 'increasing': {'line': {'color': colorscale[0]}},
-#                 'decreasing': {'line': {'color': colorscale[1]}}
-#             }
-#             bb_bands = bbands(dff['1_bid_px'])
-#             bollinger_traces = [{
-#                 'x': dff['time'], 'y': y,
-#                 'type': 'scatter', 'mode': 'lines',
-#                 'line': {'width': 1, 'color': colorscale[(i * 2) % len(colorscale)]},
-#                 'hoverinfo': 'none',
-#                 'legendgroup': ticker,
-#                 'showlegend': True if i == 0 else False,
-#                 'name': '{} - bollinger bands'.format(ticker)
-#             } for i, y in enumerate(bb_bands)]
-#             graphs.append(dcc.Graph(
-#                 id=ticker,
-#                 figure={
-#                     'data': [candlestick] + bollinger_traces,
-#                     'layout': {
-#                         'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
-#                         'legend': {'x': 0}
-#                     }
-#                 }
-#             ))
-
-#     return graphs
 
 
 if __name__ == '__main__':
